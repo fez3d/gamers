5115d29accc23be2045b62a3e45e975f06752a0c
Daniel Medina <soydanielmedina@gmail.com>
2022-10-28 16:14:55 -0500

    add basic games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def snake
+  end
+
+  def tetris
+  end
+
+  def breakout
+  end
+
+  def doodle_jump
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'games/snake'
+  get 'games/tetris'
+  get 'games/breakout'
+  get 'games/doodle_jump'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 end
