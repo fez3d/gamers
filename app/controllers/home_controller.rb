1e51dd8ef2be5beba4580c866a97874cf15eefe3
Daniel Medina <soydanielmedina@gmail.com>
2022-10-31 10:44:33 -0600

    fix already login user to redirect to profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+    redirect_to user_profile_path if current_user
+  end
+
+  #PUT /user/:id/update_score
+  def update_score
+    user = User.find(params[:id])
+
+    total_money = user.money + params[:money].to_i
+    if user.update(money: total_money)
+      render json: {
+        status: 200
+      }
+    else
+      render json: {
+        error: 'no se actualizo',
+        status: 400
+      }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'games/breakout'
   get 'games/doodle_jump'
   get 'games/index'
+  put 'user/:id/update_score', to: 'home#update_score'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
